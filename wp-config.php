1210c0745a88d69918ecc34f5e65e9c2c38ffc4b
Fandy <55258189+Fandy261@users.noreply.github.com>
2021-08-01 12:08:29 +0200

    first commit

---
+++ wp-config.php
@@ -0,0 +1,35 @@
+<?php
+
+
+define('AUTH_SALT', ';&DKzHoFw8hvmiq ,@$~Q~s"[MoPU[ypG+[#Z HS+]!Aojgn&LE^VX8G1B&BI)MT');
+
+define('DB_NAME', '1606069972_agence_immocc_bordcodmada_site');
+
+define('DB_COLLATE', 'utf8mb4_general_ci');
+
+define('AUTH_KEY', '$3moq7>PLv*sXA*n4q1XlY"urafj~WN$z{beXAC}n3K?p~C]ZVr;4mBe.oou)dRL');
+
+define('DB_PASSWORD', 'YedymdU2Nam73Z6Lj9irNOPE7JAXt7mV');
+
+define('NONCE_SALT', '>4%l:T3!:5}U`{YOARh0~}+h)d4FDW+=WuYX1r%?=HnqmBQp).w?&B`V8FCA":H3');
+
+define('NONCE_KEY', '$weZp$}.l;6B,Vufa+4Wznr|&Rve*`>XP}i+bcvwO#2ZHf_[GvGu0@haNv)>[E,=');
+
+define('LOGGED_IN_SALT', 'W$K^ub~{v61"ixhzeWo5,+Ww2g]qZ]q_s6ZE$,Jt+;~:~1>wgi*k`{PQg|&6v9lN');
+
+define('LOGGED_IN_KEY', '1hca;}z.+ >/TA7):P$(:j%A{s;}:GqOFgPSY,XSgpWqqZRysOcKef~$YH`.O J/');
+
+define('DB_USER', '1606069972_agence_immocc_bordcod');
+
+define('DB_HOST', 'localhost');
+
+define('DB_CHARSET', 'utf8mb4');
+
+define('SECURE_AUTH_KEY', '*Xl$Y/6++~1fv%q`>gI7ak&68oVu$A5A)AKDn/+&zQ)y"eC67<u8@1}Xm/O8MoD]');
+
+define('SECURE_AUTH_SALT', 'jK8/B*i++4{i</vQOz=z_"{E:7a:!bycOh:[X+vD8|DSQ{nq18((#u(I"nDe>s g');
+
+
+$table_prefix = 'wp_';
+
+require_once(ABSPATH . 'wp-settings.php');
